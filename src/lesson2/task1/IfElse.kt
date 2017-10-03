f8e479e2bd57c6e90ea565c0612006679e47e48f
PolyaVyatkina <polya.vyatkina@mail.ru>
2017-10-04 01:19:37 +0800

    The 1st tasks in the 2nd lesson is completed.

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -2,6 +2,7 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
 
 /**
  * Пример
@@ -33,7 +34,14 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    if ((age % 100 > 10) && (age % 100 < 20)) return "$age лет"
+    else return when {
+        age % 10 == 1  ->  "$age год"
+        (age % 10 > 1) && (age % 10 < 5)   ->  "$age года"
+        else  ->  "$age лет"
+    }
+}
 
 /**
  * Простая
@@ -44,7 +52,18 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+    val s1 = v1 * t1
+    val s2 = v2 * t2
+    val s3 = v3 * t3
+    val s = (s1 + s2 + s3) / 2
+
+    return when {
+        s <= s1  ->  s / v1
+        s <= (s1 + s2)  ->  t1 + (s - s1) / v2
+        else  ->  t1 + t2 + (s - s1 - s2) / v3
+    }
+}
 
 /**
  * Простая
@@ -58,6 +77,8 @@ fun timeForHalfWay(t1: Double, v1: Double,
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
                        rookX2: Int, rookY2: Int): Int = TODO()
+
+
 
 /**
  * Простая
@@ -81,7 +102,23 @@ fun rookOrBishopThreatens(kingX: Int, kingY: Int,
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    val maxR: Double
+    val R1: Double
+    val R2: Double
+
+    maxR = maxOf(a, b, c)
+    R2 = minOf(a, b, c)
+    R1 = a + b + c - maxR - R2
+
+    if (R1 + R2 < maxR) return -1
+    else return when {
+        sqr(R1) + sqr(R2) > sqr(maxR)  ->  0
+        sqr(R1) + sqr(R2) == sqr(maxR) -> 1
+        sqr(R1) + sqr(R2) < sqr(maxR) -> 2
+        else -> -1
+    }
+}
 
 /**
  * Средняя
@@ -91,4 +128,7 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
+    return if (minOf(b,d) - maxOf(a,c) >= 0) minOf(b,d) - maxOf(a,c)
+    else -1
+}
