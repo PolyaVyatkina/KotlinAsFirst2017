818124d1c079be3e269565518377a03d4d9f73b9
PolyaVyatkina <polya.vyatkina@mail.ru>
2017-10-06 00:03:36 +0800

    The 3rd lesson is almost completed.

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -60,7 +60,15 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  * Найти количество цифр в заданном числе n.
  * Например, число 1 содержит 1 цифру, 456 -- 3 цифры, 65536 -- 5 цифр.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+    var k = 0
+    var num = n
+    while (num > 0) {
+        k++
+        num /= 10
+    }
+    return if (k == 0) 1 else k
+}
 
 /**
  * Простая
@@ -68,7 +76,12 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int =
+    when {
+        n == 1  ->  1
+        n == 2  ->  1
+        else  ->  fib(n-1) + fib(n-2)
+    }
 
 /**
  * Простая
@@ -76,7 +89,15 @@ fun fib(n: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun lcm(m: Int, n: Int): Int {
+    var a = m
+    var b = n
+    while (a != b) {
+        if (a > b) a -= b
+        else b -= a
+    }
+    return m * n / a
+}
 
 /**
  * Простая
@@ -134,7 +155,15 @@ fun cos(x: Double, eps: Double): Double = TODO()
  * Поменять порядок цифр заданного числа n на обратный: 13478 -> 87431.
  * Не использовать строки при решении задачи.
  */
-fun revert(n: Int): Int = TODO()
+fun revert(n: Int): Int {
+    var number = n
+    var res = 0
+    while (number > 0) {
+        res = res * 10 + number % 10
+        number /= 10
+    }
+    return res
+}
 
 /**
  * Средняя
@@ -143,7 +172,8 @@ fun revert(n: Int): Int = TODO()
  * первая цифра равна последней, вторая -- предпоследней и так далее.
  * 15751 -- палиндром, 3653 -- нет.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
+fun isPalindrome(n: Int): Boolean =
+        (revert(n) == n)
 
 /**
  * Средняя
